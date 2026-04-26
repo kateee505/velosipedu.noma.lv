3c01c271f7fdd7c1d4a6a752a151595749af45da
kateee505 <150558093+kateee505@users.noreply.github.com>
2026-04-26 18:46:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ function calculate() {
     const days = Number(document.getElementById("days").value);
     const result = document.getElementById("result");
 
+    // Validācija
     if (!days || days <= 0) {
         result.innerHTML = "❌ Lūdzu ievadi pareizu dienu skaitu!";
+        result.style.color = "red";
         return;
     }
 
     let total = price * days;
 
+    // Atlaide
     if (days >= 7) {
-        total *= 0.85;
+        total = total * 0.85;
     }
 
     result.innerHTML = `💰 Kopējā cena: ${total.toFixed(2)} €`;
+    result.style.color = "green";
 }
